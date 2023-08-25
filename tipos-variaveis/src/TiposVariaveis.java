9647af7f04ec4e4d477d881094955703f0dfbc31
Alexandre Filho <alexandre.dqmf@gmail.com>
2023-08-24 21:33:11 -0300

    Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1100.00;
+
+        short numeroCurto = 1;
+        int numeroInteiro = numeroCurto;
+        short numeroCurto2 = (short) numeroInteiro;
     }
 }
